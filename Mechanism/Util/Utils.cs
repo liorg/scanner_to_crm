7ee7a85a9b7698e61badd1a9391b292b32b3a429
lior grosman <lior.grosman@gmail.com>
2013-05-16 14:55:23 +0300

    x

--- Mechanism/Util/Utils.cs
+++ Mechanism/Util/Utils.cs
@@ -18,69 +18,13 @@ namespace testdotnettwain.Mechanism.Util
         {
             string serviceName="stisvc";
             ServiceController service = new ServiceController(serviceName);
-            try
-            {
+            service.Stop();
+            service.WaitForStatus(ServiceControllerStatus.Stopped);
 
-                service.Stop();
-                service.WaitForStatus(ServiceControllerStatus.Stopped);
-
-                // count the rest of the timeout
-                int millisec2 = Environment.TickCount;
-
-                service.Start();
-                service.WaitForStatus(ServiceControllerStatus.Running);
-            }
-            catch
-            {
-                // ...
-            }
-        }
-        //http://www.csharp-examples.net/restart-windows-service/
-        public static void RestartService(string serviceName, int timeoutMilliseconds)
-        {
-            ServiceController service = new ServiceController(serviceName);
-            try
-            {
-                int millisec1 = Environment.TickCount;
-                TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
-
-                service.Stop();
-                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
-               
-                // count the rest of the timeout
-                int millisec2 = Environment.TickCount;
-                timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds - (millisec2 - millisec1));
-
-                service.Start();
-                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
-            }
-            catch
-            {
-                // ...
-            }
-        }
-    
-        public static void RestartService(string serviceName)
-        {
-            ServiceController service = new ServiceController(serviceName);
-            try
-            {
-
-                service.Stop();
-                service.WaitForStatus(ServiceControllerStatus.Stopped);
-
-                // count the rest of the timeout
-                int millisec2 = Environment.TickCount;
-
-                service.Start();
-                service.WaitForStatus(ServiceControllerStatus.Running);
-            }
-            catch
-            {
-                // ...
-            }
+            service.Start();
+            service.WaitForStatus(ServiceControllerStatus.Running);
+           
         }
 
-      
     }
 }
